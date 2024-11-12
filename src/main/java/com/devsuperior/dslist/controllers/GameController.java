95360f92fd63c053e545f591e8fb319a7b0656ca
Felipe Tatsuya Aso <asotatsuya0304@gmail.com>
2024-11-12 19:14:02 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dslist.controllers;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.respositories.GameRepository;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,12 @@ public class GameController {
     //o controller implementa a API
     @Autowired
     private GameService gameService; //controller injeta um service
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findAll(@PathVariable Long id){ //para casar com o id recebido se bota no @PathVariable
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping //endpoint
     public List<GameMinDTO> findAll(){
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dslist.services;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.respositories.GameRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +16,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository; //injetando uma instancia do gamerepository dentro do service
 
+    @org.springframework.transaction.annotation.Transactional(readOnly = true) //para garantir a transacao com o banco de dados
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
-        List<Game> result = gameRepository.findAll();
+        List<Game> result = gameRepository.findAll(); //chama o repository e busca todos os dados
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
